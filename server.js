78769e540fdc6355a7214d9dc5912df4b6401eba
takuya <hukujuth@gmail.com>
2023-02-16 08:04:50 +0900

    ローカル環境の変数名と一致

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const stripe = require("stripe")(
   "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"
 );
 
-const apiKey = process.env.API_KEY;
+const apiKey = process.env.VARIABLE_NAME;
 
 const app = express();
 app.use(cors());
